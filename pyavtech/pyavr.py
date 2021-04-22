50f2dd3ea19b301fe70112ae10aabf74affcba14
Laurent Bonnet <laurent.bonnet@ul.com>
2021-04-22 15:09:52 +0200

    Alignement with visa package 1.11.3 version -  Add build sphinx in setup - Add CI script

--- pyavtech/pyavr.py
+++ pyavtech/pyavr.py
@@ -1,4 +1,4 @@
-import visa
+import pyvisa as visa
 import logging
 
 
--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import setup
 from setuptools import find_packages
+from sphinx.setup_command import BuildDoc
 from os import path
+
+name = 'pyavtech'
+version = '0.1.0'
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='pyavtech',
-      version='0.1.0',
+setup(name='name',
+      version=version,
       
+      cmdclass={'build_sphinx': BuildDoc},
+      command_options={
+          'build_sphinx': {
+              'project': ('setup.py', name),
+              'version': ('setup.py', version),
+              'source_dir': ('setup.py', 'docs/source')
+          }
+      },
       description='AVTECH Device driver',
       
       long_description=long_description,
@@ -30,5 +42,5 @@ setup(name='pyavtech',
       python_requires='>=3.6',
       license='MIT',
       packages=find_packages(),
-      install_requires=['PyVISA'],
+      install_requires=['PyVISA>=1.11.3'],
       zip_safe=False)
